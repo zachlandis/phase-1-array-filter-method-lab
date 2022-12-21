748e7e3eedf1603cf9ff528e239cb0b90ccddcfb
Zach <zachlandis91@gmail.com>
2022-12-21 11:29:17 -0700

    commit tree c1f80cdf24cabe01158f7bd0b22440ad810487c3

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution here
+
+function findMatching(drivers, string) {
+    return drivers.filter(function (name) {
+        if (name.toLowerCase() === string.toLowerCase()) {
+            return name;
+        }
+    })
+}
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter(function (nameBegins) {
+        if (nameBegins.startsWith(string)) {
+            return nameBegins;
+        }
+    })
+}
+
+
+function matchName(drivers, string) {
+    return drivers.filter(function (name) {
+        if (name.name === string) {
+            return drivers;
+        }
+    })
+}
